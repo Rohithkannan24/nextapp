51d50970a057da59bc52a532b9f981698bc28568
Rohithkannan24 <rohithank24@gmail.com>
2025-10-24 16:20:45 +0530

    using DaisyUi in our Project

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,21 +3,33 @@ import React from "react";
 interface user {
   id: number;
   name: string;
+  email: string;
 }
 
 const UserPages = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-    next: { revalidate: 10 },
+    next: { revalidate: 20 },
   });
   const users: user[] = await res.json();
   return (
     <>
       <h1>Users</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
